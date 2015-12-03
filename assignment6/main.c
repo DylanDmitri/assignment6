f319db3997ffaa0ad804e11414ea917b4daaec69
DylanDmitri <d.dylan.g@gmail.com>
2015-12-03 12:07:04 -0600

    Initial Commit

---
+++ assignment6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  assignment6
+//
+//  Created by Dylan Gray on 12/3/15.
+//  Copyright © 2015 Dylan Gray. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
